264472b3b44913722805294fdea2cd99204ce2d0
ru14 <74753345+ru14@users.noreply.github.com>
2021-02-25 17:46:50 -0800

    modified html and js

--- script.js
+++ script.js
@@ -1,10 +1,39 @@
 
 
+//timer will be loop
+//1) How to create an unordered list (a list with the list items in bullets) in HTML?
+//ANSWER<ul>
+//<ol>
+//<li>
+//<i></i>
+
+//The correct sequence of HTML tags for starting a webpage is -
+//Head, Title, HTML, body
+//HTML, Body, Title, Head
+//HTML, Head, Title, Body
+//HTML, Head, Title, Body
 
 
+//	If we want define style for an unique element, then which css selector will we use ?
+//A. Id
+//B. text
+//C. class
+//D. name
+
+//1.  Why so JavaScript and Java have similar name?
+//A.  JavaScript is a stripped-down version of Java
+//B.  JavaScript's syntax is loosely based on Java's
+//C.  They both originated on the island of Java
+//D.  None of the above
+//Ans: B
 
 
-
+//2.  When a user views a page containing a JavaScript program, which machine actually executes the script?
+//A.  The User's machine running a Web browser
+//B.   The Web server
+//C.  A central machine deep within Netscape's corporate offices
+//D.  None of the above
+//Ans:  A
 
 
 
